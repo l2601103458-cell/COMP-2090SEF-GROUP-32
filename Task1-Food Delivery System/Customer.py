c8724cd3c7ca2090ca8b80a3daf7594a162f137c
l2601103458-cell <l2601103458@gmail.com>
2026-03-08 18:50:40 +0800

    Implement Customers class with account management

--- Task1-Food Delivery System/Customer.py
+++ Task1-Food Delivery System/Customer.py
@@ -1,1 +1,12 @@
-
+class Customers:
+    account_list = []
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.money = 0
+        Customers.account_list.append(self.username + ";" + self.password)
+    
+    def add_money(self):
+        money = int(input("How many you want to add: "))
+        self.money += money
+        print(self.money)
