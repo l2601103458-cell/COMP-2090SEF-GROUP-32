4adb2eb75bb9b8ba10d64b9b32ca9c2f54c33df3
l2601103458-cell <l2601103458@gmail.com>
2026-04-08 15:56:33 +0800

    Update heap.py

--- Task2-HeapSort/heap.py
+++ Task2-HeapSort/heap.py
@@ -1,1 +1,39 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
 
+    def insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap) - 1)
+
+    def _heapify_up(self, i):
+        while i > 0 and self.heap[(i-1)//2] < self.heap[i]:
+            self.heap[i], self.heap[(i-1)//2] = self.heap[(i-1)//2], self.heap[i]
+            i = (i-1)//2
+
+    def extract_max(self):
+        if not self.heap:
+            return None
+        max_val = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._heapify_down(0)
+        return max_val
+
+    def _heapify_down(self, i):
+        n = len(self.heap)
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and self.heap[left] > self.heap[largest]:
+            largest = left
+        if right < n and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != i:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self._heapify_down(largest)
+
+    def is_empty(self):
+        return len(self.heap) == 0
